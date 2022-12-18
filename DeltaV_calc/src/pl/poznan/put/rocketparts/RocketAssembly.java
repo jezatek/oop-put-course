89c19c853eb6733467dfa02564a6916269283c5b
jezatek <115163641+jezatek@users.noreply.github.com>
2022-12-18 23:43:41 +0100

    Delete DeltaV_calc directory

--- DeltaV_calc/src/pl/poznan/put/rocketparts/RocketAssembly.java
+++
@@ -1,7 +0,0 @@
-package pl.poznan.put.rocketparts;
-
-public interface RocketAssembly {
-	String deltaV();
-	void newBottomStage(Stage booster);
-	public float totalmass();
-}
--- DeltaV_calc/src/pl/poznan/put/rocketparts/Segment.java
+++
@@ -1,7 +0,0 @@
-package pl.poznan.put.rocketparts;
-
-public interface Segment {
-	Segment addedfuel(float mass);
-	String specification();
-	public float totalmass();
-}
